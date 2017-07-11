ee1bb439a31235a843de40ae3351683516974e1e
wo_fpc <wo_fpc@outlook.com>
2017-07-11 23:31:54 +0800

    finish demo

--- test/test1.lua
+++
@@ -1,8 +0,0 @@
-test = 'test1'
-local test1 = {}
-function test1.pp()
-    print(test)
-    haha()
-end
-
-return test1
--- test/test2.lua
+++
@@ -1,9 +0,0 @@
-
-
-function haha()
-    print("haha")
-end
-
-local test1 = require("test1")
-test = 'test2'
-test1.pp()
